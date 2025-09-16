44709a56b6516c942fe0a1b62d9067929e447ad8
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-09-16 17:28:45 -0300

    Adicionar interface e implementação para cópia de templates
    Foi adicionada a interface `ITemplateCopier` no arquivo `ITemplateCopier.cs`, que define o método `Copy` para copiar arquivos e pastas, removendo a extensão ".template". A implementação desse método foi feita na classe `TemplateCopier` no arquivo `TemplateCopier.cs`, que inclui verificações de existência de diretórios e cópia recursiva de arquivos e subpastas.

---
+++ src/experience-widget/Interfaces/ITemplateCopier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace experience_widget.Interfaces
+{
+    interface ITemplateCopier
+    {
+        /// <summary>
+        /// Copia todos os arquivos e pastas de sourcePath para destinationPath,
+        /// mantendo a estrutura de pastas e removendo ".template" do nome dos arquivos.
+        /// </summary>
+        /// <param name="sourcePath">Caminho da pasta de templates</param>
+        /// <param name="destinationPath">Caminho da pasta de destino</param>
+        public void Copy(string sourcePath, string destinationPath);
+    }
+}
---
+++ src/experience-widget/Services/TemplateCopier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace ExperienceWidgetCli.Services
+{
+    public class TemplateCopier
+    {
+        /// <summary>
+        /// Copia todos os arquivos e pastas de sourcePath para destinationPath,
+        /// mantendo a estrutura de pastas e removendo ".template" do nome dos arquivos.
+        /// </summary>
+        /// <param name="sourcePath">Caminho da pasta de templates</param>
+        /// <param name="destinationPath">Caminho da pasta de destino</param>
+        public void Copy(string sourcePath, string destinationPath)
+        {
+            if (!Directory.Exists(sourcePath))
+            {
+                throw new DirectoryNotFoundException($"A pasta de origem '{sourcePath}' não existe.");
+            }
+
+            // Cria a pasta de destino se não existir
+            Directory.CreateDirectory(destinationPath);
+
+            // Copia arquivos na raiz da pasta
+            foreach (var file in Directory.GetFiles(sourcePath))
+            {
+                var fileName = Path.GetFileName(file).Replace(".template", "");
+                var destFile = Path.Combine(destinationPath, fileName);
+
+                File.Copy(file, destFile, overwrite: true);
+            }
+
+            // Copia recursivamente subpastas
+            foreach (var dir in Directory.GetDirectories(sourcePath))
+            {
+                var subDirName = Path.GetFileName(dir);
+                var destSubDir = Path.Combine(destinationPath, subDirName);
+                Copy(dir, destSubDir); // chamada recursiva
+            }
+        }
+    }
+}
