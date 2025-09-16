661faa5d0d1190b834130a7fda3e63fc7b544cd4
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-09-16 16:23:42 -0300

    - Voltar para estrutura antiga - Adicionar estrutura para chamar o WidgetGenerator

--- experience-widget/Program.cs
+++ experience-widget/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ExperienceWidgetCli.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 2 || args[0].ToLower() != "create")
+        {
+            Console.WriteLine("Uso: experience-widget create <nome-do-widget>");
+            return;
+        }
+
+        var widgetName = args[1];
+        var templatesPath = Path.Combine(AppContext.BaseDirectory, "templates");
+
+        var generator = new WidgetGenerator(templatesPath);
+        generator.Generate(widgetName);
+    }
+}
