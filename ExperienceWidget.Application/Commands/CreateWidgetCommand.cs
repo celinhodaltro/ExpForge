e8b7c855dbbdd58ebb964c97e0bcf9a964bf209d
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-09-18 14:24:36 -0300

    ajuste

--- ExperienceWidget.Application/Commands/CreateWidgetCommand.cs
+++ ExperienceWidget.Application/Commands/CreateWidgetCommand.cs
@@ -5,8 +5,15 @@ namespace ExperienceWidget.Application.Commands
 {
     public class CreateWidgetCommand : IRequest<bool>
     {
+        public CreateWidgetCommand(string widgetName, string templateName, string? destinationPath = null)
+        {
+            WidgetName = widgetName;
+            TemplateName = templateName;
+            DestinationPath = destinationPath;
+        }
+
         public string WidgetName { get; set; }
         public string TemplateName { get; set; }
-        public string DestinationPath { get; set; }
+        public string? DestinationPath { get; set; }
     }
 }
