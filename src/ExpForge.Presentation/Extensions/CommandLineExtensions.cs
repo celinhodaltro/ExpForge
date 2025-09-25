543ab5fb268577d7f0e3b903a1915d73e0ccd4e5
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2025-09-25 10:57:40 -0300

    Ajustar Presentation project para ter extensions para tirar a verbosidade extensa

---
+++ src/ExpForge.Presentation/Extensions/CommandLineExtensions.cs
@@ -0,0 +1,32 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+
+public static class CommandLineExtensions
+{
+    public static string GetValueOrPrompt(
+        this CommandLineApplication app,
+        string currentValue,
+        string promptMessage,
+        Func<string> validateOrResolve,
+        string errorMessageIfEmpty = "❌ Value cannot be empty.")
+    {
+        if (!string.IsNullOrWhiteSpace(currentValue))
+            return currentValue;
+
+        var resolved = validateOrResolve?.Invoke();
+        if (!string.IsNullOrWhiteSpace(resolved))
+            return resolved;
+
+        var input = Prompt.GetString(promptMessage);
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            app.Error.WriteLine(errorMessageIfEmpty);
+            return null;
+        }
+
+        return input;
+    }
+
+
+
+}
--- src/ExpForge.Presentation/Program.cs
+++ src/ExpForge.Presentation/Program.cs
@@ -1,68 +1,32 @@
-﻿using ExpForge.Application.Behaviors;
-using ExpForge.Application.Commands;
-using ExpForge.Application.Interfaces;
-using ExpForge.CLI.Actions;
+﻿using ExpForge.CLI.Actions;
+using ExpForge.CLI.Extensions;
 using ExpForge.CLI.Services;
-using ExpForge.Infrastructure.Providers;
-using ExpForgeCli.Services;
-using FluentValidation;
 using McMaster.Extensions.CommandLineUtils;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.IO;
 
 class Program
 {
     static int Main(string[] args)
     {
-
-        var services = new ServiceCollection();
-        ConfigureServices(services);
-
-        var serviceProvider = services.BuildServiceProvider();
+        var services = new ServiceCollection()
+            .AddLoggingConfiguration()
+            .AddApplicationServices()
+            .BuildServiceProvider();
 
         var app = new CommandLineApplication<MainAction>();
         app.Conventions
            .UseDefaultConventions()
-           .UseConstructorInjection(serviceProvider);
+           .UseConstructorInjection(services);
 
         try
         {
             return app.Execute(args);
         }
-        catch (UnrecognizedCommandParsingException ex)
+        catch (UnrecognizedCommandParsingException)
         {
             TerminalMessageService.WriteLine($"Error: Unrecognized command or argument:", MessageStatus.Error);
             return 1;
         }
-    }
-
-    static void ConfigureServices(IServiceCollection services)
-    {
-        services.AddLogging(cfg =>
-        {
-            cfg.AddConsole();
-            cfg.AddFilter("LuckyPennySoftware.MediatR.License", LogLevel.None);
-        });
-
-        var applicationAssembly = typeof(ExpForge.Application.Commands.CreateWidgetCommand).Assembly;
-        services.AddValidatorsFromAssembly(typeof(CreateWidgetCommand).Assembly);
-
-        // pipeline behavior
-        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
-        services.AddScoped<ITemplatePathProvider, TemplatePathProvider>();
-
-
-        services.AddMediatR(cfg =>
-        {
-            cfg.RegisterServicesFromAssembly(applicationAssembly);
-        });
-
-        var templatesPath = Path.Combine(AppContext.BaseDirectory, "templates");
-
-        services.AddSingleton<CreateWidgetService>();
     }
 }
